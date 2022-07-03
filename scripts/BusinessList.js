d0d49245e2f8894a5a54195d588e6eaff27e4a42
Jeannette Geib <jeannettegeib@gmail.com>
2022-07-03 10:26:48 -0400

    Commiting from Laptop so I can test it on desk computer

---
+++ scripts/BusinessList.js
@@ -0,0 +1,19 @@
+import { getBusinesses } from "./database.js"
+
+export const BusinessList=()=>{
+    const businessesArray=getBusinesses();
+    let businessesListHTML = "<h1>Active Businesses</h1>";
+    
+    businessesArray.forEach(
+        (businessObject)=>{
+            businessesListHTML +=`
+            <section class="business">
+               <h2 class="business--name">${businessObject.companyName}</h2}
+               <div class="business--details">${businessObject.addressFullStreet}<br>${addressCity} ${addressStateCode} ${addressZipCode}</div>
+               <hr>
+               </section>
+               `
+        }
+    )
+    return businessesListHTML
+} 
